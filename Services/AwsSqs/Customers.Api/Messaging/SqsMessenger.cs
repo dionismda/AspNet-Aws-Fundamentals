665dbfa0d5e602ad5b9ffae3de7d5859b3cc6314
dionismda <dionismda@gmail.com>
2023-06-05 13:05:45 -0300

    Finishing sqsservice

---
+++ Services/AwsSqs/Customers.Api/Messaging/SqsMessenger.cs
@@ -0,0 +1,53 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace Customers.Api.Messaging;
+
+public class SqsMessenger : ISqsMessenger
+{
+    private readonly IAmazonSQS _amazonSQS;
+    private readonly QueueSettings _queueSettings;
+    private string? _queueUrl;
+
+    public SqsMessenger(IAmazonSQS amazonSQS, IOptions<QueueSettings> queueSettings)
+    {
+        _amazonSQS = amazonSQS;
+        _queueSettings = queueSettings.Value;
+    }
+
+    public async Task<SendMessageResponse> SendMessageAsync<TMessage>(TMessage message)
+    {
+        var sendMessageResquest = new SendMessageRequest
+        {
+            QueueUrl = await GetQueueUrlAsync(),
+            MessageBody = JsonSerializer.Serialize(message),
+            MessageAttributes = new Dictionary<string, MessageAttributeValue>
+            {
+                {
+                    "MessageType", new MessageAttributeValue
+                    {
+                        DataType = "String",
+                        StringValue = typeof(TMessage).Name
+                    }
+                }
+            }
+        };
+
+        return await _amazonSQS.SendMessageAsync(sendMessageResquest);
+    }
+
+    private async Task<string> GetQueueUrlAsync()
+    {
+        if (_queueUrl is not null)
+        {
+            return _queueUrl;
+        }
+
+        var queueResponse = await _amazonSQS.GetQueueUrlAsync(_queueSettings.QueueName);
+        _queueUrl = queueResponse.QueueUrl;
+
+        return _queueUrl;
+    }
+}
---
+++ Services/AwsSqs/Customers.Consumer/QueueConsumerService.cs
@@ -0,0 +1,72 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using Customers.Consumer.Messages;
+using MediatR;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace Customers.Consumer;
+
+public class QueueConsumerService : BackgroundService
+{
+    private readonly IAmazonSQS _amazonSQS;
+    private readonly QueueSettings _queueSettings;
+    private readonly IMediator _mediator;
+    private readonly ILogger<QueueConsumerService> _logger;
+
+    public QueueConsumerService(IAmazonSQS amazonSQS,
+                                IOptions<QueueSettings> queueSettings,
+                                IMediator mediator,
+                                ILogger<QueueConsumerService> logger)
+    {
+        _amazonSQS = amazonSQS;
+        _queueSettings = queueSettings.Value;
+        _mediator = mediator;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var queueUrlResponse = await _amazonSQS.GetQueueUrlAsync(_queueSettings.QueueName, stoppingToken);
+
+        var receiveMessageRequest = new ReceiveMessageRequest
+        {
+            QueueUrl = queueUrlResponse.QueueUrl,
+            AttributeNames = new List<string> { "All" },
+            MessageAttributeNames = new List<string> { "All" },
+            MaxNumberOfMessages = 10
+        };
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var response = await _amazonSQS.ReceiveMessageAsync(receiveMessageRequest, stoppingToken);
+
+            foreach (var message in response.Messages)
+            {
+                var messageType = message.MessageAttributes["MessageType"].StringValue;
+                var type = Type.GetType($"Customers.Consumer.Messages.{messageType}");
+                if (type is null)
+                {
+                    _logger.LogWarning("Unknown message type: {MessageType}", messageType);
+                    continue;
+                }
+
+                var typedMessage = (ISqsMessage)JsonSerializer.Deserialize(message.Body, type)!;
+
+                try
+                {
+                    await _mediator.Send(typedMessage, stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Message failed during processing");
+                    continue;
+                }
+
+                await _amazonSQS.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle, stoppingToken);
+            }
+
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
