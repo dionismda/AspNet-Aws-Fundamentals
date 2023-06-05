76308ad6c1929afa7741655c0c5ef72e46c8180c
dionismda <dionismda@gmail.com>
2023-06-05 10:08:38 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Fundamentos Aws - AspNetCore
+
+Dando o seu primeiro passo no mundo da computação em nuvem?
+
+A AWS é o provedor de nuvem líder, com uma margem bastante ampla, e, ao contrário da crença popular, é uma escolha excelente para os seus aplicativos C# e .NET. Neste repositório, você encontrará exemplos básicos de implementação de todos os serviços da AWS que você precisa saber como utilizar na AWS.
+
+# Serviços usados
+
+* [Sqs](https://aws.amazon.com/pt/sqs/) :
+O Amazon Simple Queue Service (SQS) permite que você envie, armazene e receba mensagens entre componentes de software em qualquer volume, sem perder mensagens ou precisar que outros serviços estejam disponíveis.
+
+
+# Licença
+O PratoFeito é distribuído sob a licença MIT. Para mais informações, consulte o arquivo LICENSE neste repositório.
+Espero que isso ajude! Se você tiver mais dúvidas, sinta-se à vontade para perguntar.
---
+++ Services/AwsSqs/SqsPublisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+using System.Text.Json;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "teste@teste.com.br",
+    FullName = "Teste",
+    DateOfBirth = new DateTime(1991, 5, 25),
+    GitHubUsername = "teste",
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+await sqsClient.SendMessageAsync(sendMessageRequest);
